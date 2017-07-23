a6de34c154c4532e6432dd1ff7b22baac8dc4a2d
Kelly Ng <kellyng87@gmail.com>
2017-07-22 23:00:18 -0700

    Added controlled component for search input

--- src/Search.js
+++ src/Search.js
@@ -1,17 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
-const Search = () => {
+
+class Search extends Component {
+
+  state = {
+    books: [],
+    query: ''
+  }
+
+  handleChange = (value) => {
+    this.setState({
+      query: value
+    })
+    //console.log(this.state.query);
+  }
+
+  render() {
     return(
         <div className='search-books'>
           <div className='search-books-bar'>
             <Link to="/" className='close-search'>Back to Main Page</Link>
             <div className='search-books-input-wrapper'>
-              <input type="text" name="book" placeholder="Book Name"/>
+              <input type="text"
+                name="book"
+                value={this.state.query}
+                onChange={(event) => this.handleChange(event.target.value)}
+                placeholder="Search by title or author"
+              />
             </div>
+          </div>
+          <div className='search-books-results'>
+            <h1>List Books Component?</h1>
           </div>
         </div>
     )
+  }
 }
+
 
 export default Search
