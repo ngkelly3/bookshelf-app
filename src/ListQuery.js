b13a72df14588fb1ed489c26449878cae3aae84e
Kelly Ng <kellyng87@gmail.com>
2017-07-25 16:21:14 -0700

    Refactor search UI component

--- src/ListQuery.js
+++ src/ListQuery.js
@@ -1,79 +1,20 @@
 import React, { Component } from 'react'
+import ListBooks from './ListBooks'
 import PropTypes from 'prop-types'
 import * as BooksAPI from './BooksAPI'
 
-class ListQuery extends Component {
+const ListQuery = (props) => {
 
-  static propTypes = {
-    books: PropTypes.array.isRequired
-  }
+  // static propTypes = {
+  //   books: PropTypes.array.isRequired
+  // }
 
-  handleShelfChange = (event, book) => {
-    let shelf = event.target.value;
-    // change
-    BooksAPI.update(book, shelf).then(
-      this.props.updateBooks(book, shelf)
-    );
-    console.log(shelf);
-    // console.log(this.state.books);
-
-  }
-
-
-  render() {
-    // props destructuring
-    const { books } = this.props;
-
-    // represents the books of the current shelf
-    let thisBookShelf = books;
-
-    return(
-            <div className='bookshelf-books'>
-              {
-                (thisBookShelf !== undefined) ?
-                  <ol className='books-grid'>
-                    {thisBookShelf.map((book) => (
-                      <li key={book.id}>
-                        <div className='book'>
-                          <div className='book-top'>
-                          {
-                            (book.imageLinks !== undefined) ?
-                              <div className='book-cover' style={{
-                                backgroundImage: `url(${book.imageLinks.thumbnail})`,
-                                width: 128,
-                                height: 192
-                              }}/> : <div></div>
-                          }
-                            <div className='book-shelf-changer'>
-                              <select value={book.shelf} onChange={(e) => this.handleShelfChange(e, book)}>
-                                <option value='none' disabled>Move to...</option>
-                                <option value='currentlyReading'>Currently Reading</option>
-                                <option value='wantToRead'>Want to Read</option>
-                                <option value='read'>Read</option>
-                                <option value='none'>None</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div className='book-title'>
-                            <h5>{book.title}</h5>
-                          </div>
-                          {
-                            (book.authors !== undefined) ?
-                            <div className='book-authors'>
-                              {book.authors.map((author) => (
-                                <div className='book-authors' key={author.toString()}>{author}</div>
-                              ))}
-                            </div> : <div></div>
-                          }
-                        </div>
-                      </li>
-                    ))}
-                  </ol> : <div></div>
-              }
-            </div>
-    )
-
-  }
+  // props destructuring
+  const { books, updateBooks } = props;
+  // represents the books of the current shelf
+  return(
+    <ListBooks books={ books } updateBooks={ updateBooks }/>
+  )
 
 }
 
